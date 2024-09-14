4bee1ef517f28c886cdaa712155f559b67e4dd72
Mohammed Amer <loard.amer.2010@gmail.com>
2024-09-14 15:09:45 +0300

    used color modulation there's something missing but I will go on now

--- main.cpp
+++ main.cpp
@@ -11,13 +11,10 @@ int main( int argc, char** argv )
 	SDL_Renderer* window_renderer = NULL;
 	SDL_Texture* window_texture1 = NULL;
 	SDL_Texture* window_texture2 = NULL;
-	SDL_Rect arr[4];
-	SDL_Rect renderQuad;
 
-	arr[0] = {15, 15, 104, 86};
-	arr[1] = {126, 29, 69, 72};
-	arr[2] = {13, 110, 153, 77};
-	arr[3] = {207, 30, 99, 69};
+	Uint8 R = 255;
+	Uint8 G = 255;
+	Uint8 B = 255;
 
 
     if (init(&main_window, &window_renderer) == false)
@@ -45,38 +42,37 @@ int main( int argc, char** argv )
 					{
 						quite = true;
 					}
-					else
+					else if (e.type == SDL_KEYDOWN)
 					{
+						switch (e.key.keysym.sym)
+						{
+						case SDLK_w:
+							R += 70;
+							break;
+						case SDLK_s:
+							R -= 70;
+							break;
+						case SDLK_d:
+							G += 70;
+							break;
+						case SDLK_a:
+							G -= 70;
+							break;
+						case SDLK_z:
+							B += 70;
+							break;
+						case SDLK_r:
+							B -= 70;
+							break;
+						default:
+							break;
+						}
+					}
 						SDL_SetRenderDrawColor(window_renderer,0xff, 0xff, 0xff, 0xff);
 						SDL_RenderClear(window_renderer);
-						renderQuad.x = arr[0].x;
-						renderQuad.y = arr[0].y;
-						renderQuad.w = arr[0].w * 2;
-						renderQuad.h = arr[0].h * 2;
-
-						SDL_RenderCopy(window_renderer, window_texture1, &arr[0], &renderQuad);
-						renderQuad.x = SCREEN_WIDTH - arr[1].w * 2;
-						renderQuad.y = arr[1].y;
-						renderQuad.w = arr[1].w * 2;
-						renderQuad.h = arr[1].h * 2;
-
-						SDL_RenderCopy(window_renderer, window_texture1, &arr[1], &renderQuad);
-
-						renderQuad.x = arr[2].x;
-						renderQuad.y = SCREEN_HEIGHT - arr[2].h * 2;
-						renderQuad.w = arr[2].w * 2;
-						renderQuad.h = arr[2].h * 2;
-
-						SDL_RenderCopy(window_renderer, window_texture1, &arr[2], &renderQuad);
-
-						renderQuad.x = SCREEN_WIDTH - arr[3].w * 2;
-						renderQuad.y = SCREEN_HEIGHT - arr[3].h * 2;
-						renderQuad.w = arr[3].w * 2;
-						renderQuad.h = arr[3].h * 2;
-
-						SDL_RenderCopy(window_renderer, window_texture1, &arr[3], &renderQuad);
+						SDL_SetTextureColorMod(window_texture1, R, G, B);
+						SDL_RenderCopy(window_renderer, window_texture1, NULL, NULL);
 						SDL_RenderPresent(window_renderer);
-					}
 				}
 			}
 			close(main_window, window_renderer, window_texture1, window_texture2);
